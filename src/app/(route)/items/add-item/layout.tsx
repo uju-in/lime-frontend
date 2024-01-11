ff83194f7ef65f1954ff8bedc583074d10c03aeb
mintmin <mintmin0320@gmail.com>
2024-01-12 01:59:41 +0900

    style: Layout 적용

--- src/app/(route)/items/add-item/layout.tsx
+++ src/app/(route)/items/add-item/layout.tsx
@@ -1,11 +1,15 @@
+import Layout from '@/app/_components/layout/Layout'
+
 export default function AddItemLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="mx-auto flex h-dvh w-dvw max-w-[1440px] justify-center bg-[#F7F7F7]">
-      {children}
-    </div>
+    <Layout>
+      <div className="mx-auto flex h-dvh max-w-[1440px] justify-center bg-[#F7F7F7]">
+        {children}
+      </div>
+    </Layout>
   )
 }
