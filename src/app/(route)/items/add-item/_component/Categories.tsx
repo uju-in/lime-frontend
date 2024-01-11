d74c4dc2695ce0c89d883047862023fc5f142a7c
mintmin <mintmin0320@gmail.com>
2024-01-12 00:56:04 +0900

    design: font 조정

--- src/app/(route)/items/add-item/_component/Categories.tsx
+++ src/app/(route)/items/add-item/_component/Categories.tsx
@@ -10,7 +10,7 @@ export default function Categories({ categories }: { categories: string[] }) {
       {categories.map((item: string) => (
         <label
           key={item}
-          className={`my-4 mr-6 inline-block cursor-pointer rounded-full border px-5 py-2 text-lg font-medium ${
+          className={`my-4 mr-6 inline-block cursor-pointer rounded-full border px-5 py-1 font-[600] ${
             selectedItem === item
               ? 'border-black'
               : 'border-[#DADADA] text-[#898989]'
