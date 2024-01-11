84ba8253ef28c47493edb3ecdc5a99b9e0e5df3a
mintmin <mintmin0320@gmail.com>
2024-01-12 00:19:38 +0900

    feat: 취미 선택 radio button 추가

---
+++ src/app/(route)/items/add-item/_component/Categories.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useState } from 'react'
+
+export default function Categories({ categories }: { categories: string[] }) {
+  const [selectedItem, setSelectedItem] = useState('')
+  return (
+    <div className="flex flex-wrap">
+      {categories.map((item: string) => (
+        <label
+          key={item}
+          className={`my-4 mr-6 inline-block cursor-pointer rounded-full border px-5 py-2 text-lg font-medium ${
+            selectedItem === item
+              ? 'border-black'
+              : 'border-[#DADADA] text-[#898989]'
+          }`}
+        >
+          <input
+            type="radio"
+            name="category"
+            value={item}
+            checked={selectedItem === item}
+            onChange={() => setSelectedItem(item)}
+            className="sr-only"
+          />
+          {item}
+        </label>
+      ))}
+    </div>
+  )
+}
