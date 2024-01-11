c89e19f7312998ea67996025bce8ba22998c618c
mintmin <mintmin0320@gmail.com>
2024-01-12 00:56:04 +0900

    design: font 조정

--- src/app/(route)/items/add-item/page.tsx
+++ src/app/(route)/items/add-item/page.tsx
@@ -4,7 +4,7 @@ export default function AddItemPage() {
   return (
     <section className="w-[850px] flex-col bg-white px-8">
       <div className="flex flex h-[150px] items-center">
-        <h1 className="text-3xl font-bold">아이템 생성</h1>
+        <h1 className="text-[32px] font-bold">아이템 생성</h1>
       </div>
       <PostForm />
     </section>
