3a09df77cbc2be14005463d0e035beb804f1a44e
mintmin <mintmin0320@gmail.com>
2024-01-12 00:18:07 +0900

    feat: 아이템 등록 페이지 추가

---
+++ src/app/(route)/items/add-item/page.tsx
@@ -0,0 +1,9 @@
+export default function AddItemPage() {
+  return (
+    <section className="w-[850px] flex-col bg-white px-8">
+      <div className="flex flex h-[150px] items-center">
+        <h1 className="text-3xl font-bold">아이템 생성</h1>
+      </div>
+    </section>
+  )
+}
