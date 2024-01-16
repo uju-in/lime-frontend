fda6fbcde42154c2ca5fb129ac04a93d6bfc9798
mintmin <mintmin0320@gmail.com>
2024-01-16 20:48:34 +0900

    design: 전체 height 값 수정

--- src/app/(route)/join/page.tsx
+++ src/app/(route)/join/page.tsx
@@ -5,8 +5,8 @@ import UserHobbyForm from './_components/UserHobbyForm'
 
 export default function JoinPage() {
   return (
-    <main className="min-h-screen bg-[#F7F7F7]">
-      <form className="mx-auto w-[610px] bg-white px-[87px] pt-[71px]">
+    <main className="h-dvh bg-[#F7F7F7]">
+      <form className="mx-auto h-full w-[610px] bg-white px-[87px] pt-[71px]">
         <p className="mb-[10px] text-[26px] font-[700]">Lime</p>
         <h1 className="mb-[69px] text-[30px] font-[600]">프로필 작성</h1>
         <UserInfoForm />
