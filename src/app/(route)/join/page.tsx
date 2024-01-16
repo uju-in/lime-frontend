d0b0b51d23742f15add36fc1626d796b0383756d
mintmin <mintmin0320@gmail.com>
2024-01-16 20:30:32 +0900

    design: 회원가입-1 유저 정보 UI 구현

---
+++ src/app/(route)/join/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import UserInfoForm from './_components/UserInfoForm'
+
+export default function JoinPage() {
+  return (
+    <main className="min-h-screen bg-[#F7F7F7]">
+      <form className="mx-auto w-[610px] bg-white px-[87px] pt-[71px]">
+        <p className="mb-[10px] text-[26px] font-[700]">Lime</p>
+        <h1 className="mb-[69px] text-[30px] font-[600]">프로필 작성</h1>
+        <UserInfoForm />
+      </form>
+    </main>
+  )
+}
