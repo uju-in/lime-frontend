ea309daf053ff15b27b344371c0b0f318121fb50
yerim <pppp5097@naver.com>
2024-01-22 04:44:45 +0900

    feat: next config에 이미지 hostname 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shop-phinf.pstatic.net',
+        port: '',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
