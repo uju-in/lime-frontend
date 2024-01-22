202f319f747d52d5978fc5c7cf7e7f2966567950
mintmin <mintmin0320@gmail.com>
2024-01-22 16:23:34 +0900

    feat: 쿼리스트링 토큰 로컬스토리지 저장

--- src/app/(route)/join/_components/UserInfoField.tsx
+++ src/app/(route)/join/_components/UserInfoField.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { useSignUp } from '@/app/_hook/api/useSignUp'
 import { useNicknameValidation } from '@/app/_hook/api/useNicknameValidation'
+import useGetSearchParam from '@/app/_hook/common/useGetSearchParams'
 
 import { SignUpState } from '@/app/_types/signUp.types'
 
@@ -13,6 +14,7 @@ import CareerSelector from './CareerSelector'
 
 export default function UserInfoField() {
   const router = useRouter()
+  const accessToken = useGetSearchParam('accessToken')
 
   const { mutateAsync: verifyUniqueNickname } = useNicknameValidation()
   const { mutateAsync: signUp } = useSignUp()
@@ -25,6 +27,13 @@ export default function UserInfoField() {
     content: '',
     hobby: '',
   })
+
+  /* query string 토큰 저장 */
+  useEffect(() => {
+    if (accessToken) {
+      localStorage.setItem('accessToken', accessToken)
+    }
+  }, [])
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
