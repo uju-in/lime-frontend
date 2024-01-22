bd6bf4bfd7e57e7fff24d6295cd2b28805fb007f
mintmin <mintmin0320@gmail.com>
2024-01-22 22:51:31 +0900

    design: 간격 및 너비 조정

--- src/app/(route)/items/add-item/_component/PostForm.tsx
+++ src/app/(route)/items/add-item/_component/PostForm.tsx
@@ -9,19 +9,11 @@ export default function PostForm() {
         아이템 추가할 취미를 선택해 주세요.
       </p>
       <Categories />
-      <p className="mb-6 mt-12 text-[18px] font-[800px]">
+      <p className="my-12 text-[18px] font-[600]">
         아이템 추가할 URL을 입력하세요
       </p>
-      <div className="flex  w-[690px] justify-between">
-        <input className="h-[40px] w-[600px] rounded-[2px] border border-[#DADADA] p-1 focus:outline-none" />
-        <button
-          className="h-[40px] w-[65px] rounded-[2px] bg-black text-[15px] text-white"
-          type="button"
-        >
-          입력
-        </button>
-      </div>
-      <div className=" mt-[65px] flex justify-center">
+      <input className="h-[40px] w-[720px] rounded-[2px] border border-[#DADADA] p-1 focus:outline-none" />
+      <div className=" mt-[70px] flex justify-center">
         <button
           className="h-[45px] w-[250px] rounded-[2px] bg-black text-[15px] text-white"
           type="submit"
