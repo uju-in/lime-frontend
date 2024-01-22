1574dc88986235b5e1801fe670c613e686ba9ff2
mintmin <mintmin0320@gmail.com>
2024-01-23 01:56:19 +0900

    feat: 로그인 시 토큰 저장 로직 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Layout from '@/app/_components/layout/Layout'
+import { GetSearchParams } from './_components/GetSearchParams'
 
 export default function Home() {
-  return <Layout>Main</Layout>
+  return (
+    <Layout>
+      {/* 로그인 시 토큰 저장을 위해 임시로 배치한 클라이언트 컴포넌트 */}
+      <GetSearchParams />
+    </Layout>
+  )
 }
