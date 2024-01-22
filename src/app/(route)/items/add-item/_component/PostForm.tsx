85739412bdeda2617240a2e8cb49504a040789d4
mintmin <mintmin0320@gmail.com>
2024-01-22 23:02:21 +0900

    feat: 아이템 등록 변수 state 추가

--- src/app/(route)/items/add-item/_component/PostForm.tsx
+++ src/app/(route)/items/add-item/_component/PostForm.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
+
+import React, { ChangeEvent, useState } from 'react'
+
+import { ItemState } from '@/app/_types/addItem.type'
 
 import Categories from '../../../../_components/categorySelector'
 
 export default function PostForm() {
+  const [item, setItem] = useState<ItemState>({
+    hobbyValue: '',
+    itemUrl: '',
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+
+    setItem((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
   return (
     <form>
       <p className="text-xl font-semibold">
@@ -12,7 +30,11 @@ export default function PostForm() {
       <p className="my-12 text-[18px] font-[600]">
         아이템 추가할 URL을 입력하세요
       </p>
-      <input className="h-[40px] w-[720px] rounded-[2px] border border-[#DADADA] p-1 focus:outline-none" />
+      <input
+        name="itemUrl"
+        className="h-[40px] w-[720px] rounded-[2px] border border-[#DADADA] p-1 focus:outline-none"
+        onChange={handleChange}
+      />
       <div className=" mt-[70px] flex justify-center">
         <button
           className="h-[45px] w-[250px] rounded-[2px] bg-black text-[15px] text-white"
---
+++ src/app/_types/addItem.type.ts
@@ -0,0 +1,4 @@
+export interface ItemState {
+  hobbyValue: string
+  itemUrl: string
+}
