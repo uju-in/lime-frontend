23d06a38e100ad759046d7765730c92748907fa4
yerim <pppp5097@naver.com>
2024-01-24 01:20:35 +0900

    feat: 공용 모달 컨테이너 추가

---
+++ src/app/_components/modal/index.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+
+type ModalProps = {
+  children: ReactNode
+  onClose: () => void
+}
+
+function Modal({ children, onClose }: ModalProps) {
+  return (
+    <div
+      id="ModalContainer"
+      className="fixed bottom-0 left-0 right-0 top-0 z-[500] h-screen min-h-screen w-full bg-[rgba(0,0,0,0.5)]"
+    >
+      <div
+        id="ModalInner"
+        className="absolute left-[50%] top-[50%] max-w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-[8px] bg-white"
+      >
+        <button
+          type="button"
+          aria-label="close"
+          className="absolute right-[15px] top-[20px] w-[20px]"
+          onClick={() => {
+            onClose()
+          }}
+        >
+          <Image
+            width={36}
+            height={36}
+            className=""
+            src="/image/icon/icon-close.svg"
+            alt="close"
+          />
+        </button>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Modal
