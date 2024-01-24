b921d79d95693ccd32f89e6a0d34e1f26a7f723a
yerim <pppp5097@naver.com>
2024-01-24 19:46:06 +0900

    design: 찜목록 UI 수정사항 반영

--- src/app/(route)/saves/page.tsx
+++ src/app/(route)/saves/page.tsx
@@ -2,13 +2,29 @@ import React from 'react'
 import Image from 'next/image'
 
 import Layout from '@/app/_components/layout/Layout'
+import { Item } from '../items/_components/ItemList'
 
 export default function page() {
   return (
     <Layout>
       <section className="h-full px-[150px]">
-        <h1 className="mb-5 text-[36px] font-[700]">세이브 목록</h1>
-        <div className="flex">
+        <div className="mb-[56px] flex items-center justify-between">
+          <h1 className="text-[30px] font-[700]">찜목록</h1>
+          <button
+            className="flex items-center justify-center rounded-[97.6px] bg-[#242424] p-[10px_16px]"
+            type="button"
+          >
+            <p className="text-[14px] font-[600] text-white">폴더 추가하기</p>
+            <Image
+              className="ml-1 cursor-pointer"
+              width={20}
+              height={20}
+              src="/image/icon/icon-white_plus.svg"
+              alt="plus"
+            />
+          </button>
+        </div>
+        <div className="mb-[40px] flex">
           <div className="relative mr-5 flex h-[232px] w-[350px]">
             <div className="z-0 h-[232px] w-[270px] rounded-l-[8.83px] bg-[#D2D2D2]" />
             <div className="z-0">
@@ -42,20 +58,10 @@ export default function page() {
             </div>
           </div>
         </div>
-        <div className="mt-12 flex justify-center">
-          <button
-            className="flex h-[43px] w-[153px] items-center justify-center rounded-[97.6px] bg-[#242424]"
-            type="button"
-          >
-            <p className="font-[600] text-white">폴더 추가하기</p>
-            <Image
-              className="ml-1 cursor-pointer"
-              width={20}
-              height={20}
-              src="/image/icon/icon-white_plus.svg"
-              alt="plus"
-            />
-          </button>
+        <div className="flex gap-[19px]">
+          <Item />
+          <Item />
+          <Item />
         </div>
       </section>
     </Layout>
