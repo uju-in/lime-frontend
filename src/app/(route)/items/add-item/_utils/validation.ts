2737f6cfa32d1366b528bdd6de8b49d8fd5fecc4
mintmin <mintmin0320@gmail.com>
2024-01-24 16:48:51 +0900

    feat: smartstore.naver.com 정규 표현식 조건 추가

--- src/app/(route)/items/add-item/_utils/validation.ts
+++ src/app/(route)/items/add-item/_utils/validation.ts
@@ -1,10 +1,10 @@
 /* url 검증 - 다나와, 네이버, 쿠팡만 허용 */
 const validateUrl = (url: string) => {
   const urlPattern =
-    /^(https?:\/\/)?(www\.)?(shopping\.naver\.com|prod\.danawa\.com|coupang\.com)/
+    /^(https?:\/\/)?(www\.)?(shopping\.naver\.com|prod\.danawa\.com|coupang\.com|smartstore\.naver.com)/
 
   if (!urlPattern.test(url)) {
-    alert('쿠팡, 네이버 쇼핑, 다나와만 사용 가능합니다!')
+    alert('사용 가능한 URL을 입력해 주세요.')
 
     return false
   }
