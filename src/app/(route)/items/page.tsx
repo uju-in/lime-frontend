6da30151369b68ccbbf05f04842d1b09152775ec
yerim <pppp5097@naver.com>
2024-01-29 19:42:07 +0900

    style: RQProvider 위치 이동

--- src/app/(route)/items/page.tsx
+++ src/app/(route)/items/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '@/app/_components/layout/Layout'
+import RQProvider from '@/app/_components/RQProvider'
 import SideMenu from './_components/SideMenu'
 import ItemList from './_components/ItemList'
 
@@ -9,7 +10,9 @@ export default function page() {
       <div className="flex gap-[50px] px-[150px]">
         <SideMenu />
         <div className="flex-1">
-          <ItemList />
+          <RQProvider>
+            <ItemList />
+          </RQProvider>
         </div>
       </div>
     </Layout>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import RQProvider from './_components/RQProvider'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,9 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <RQProvider>{children}</RQProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
