69f12eecf035034a895b3238e86433aab2494e17
mintmin <mintmin0320@gmail.com>
2024-01-30 17:15:46 +0900

    style: 날짜 포멧 출력 수정

--- src/app/(route)/items/[itemID]/_utils/dateFormatter.ts
+++ src/app/(route)/items/[itemID]/_utils/dateFormatter.ts
@@ -1,14 +1,13 @@
-/** 'YYYY.MM.DD' 형식으로 변환 */
-
 export function dateFormatter(dateString: string): string {
   const date = new Date(dateString)
 
-  return new Intl.DateTimeFormat('ko-KR', {
+  const formattedDate = new Intl.DateTimeFormat('ko-KR', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
-  })
-    .format(date)
-    .replace(/\./g, '')
-    .replace(/(\d{4})(\d{2})(\d{2})/, '$1.$2.$3')
+  }).format(date)
+
+  return formattedDate
+    .replace(/(\d{4})\. (\d{2})\. (\d{2})\./, '$1.$2.$3')
+    .trim()
 }
