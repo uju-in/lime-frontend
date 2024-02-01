dbd0c1e1497aa4a63e2dae1a469b4a85e6e309e2
yerim <pppp5097@naver.com>
2024-02-02 00:53:32 +0900

    design: 찜목록 상세 페이지 옵션 선택 UI 구현

--- src/app/(route)/saves/[category]/page.tsx
+++ src/app/(route)/saves/[category]/page.tsx
@@ -1,28 +1,66 @@
-import React from 'react'
+'use client'
+
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
 import Layout from '@/app/_components/layout/Layout'
+import useOutsideClick from '@/app/_hook/common/useOutsideClick'
 import { Item } from '../../items/_components/ItemList'
 
-export default function page() {
+export default function SavesDetailPage() {
+  const dropdownRef = useRef(null)
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false)
+
+  /** 외부 클릭 시 dropdown 닫기 */
+  useOutsideClick(dropdownRef, () => {
+    if (isDropdownOpen) {
+      setIsDropdownOpen(false)
+    }
+  })
+
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center gap-[20px]">
-        <div className="text-[30px] font-bold">농구</div>
-        <div className="flex items-center gap-[10px] text-[14px]">
-          <div className="flex cursor-pointer gap-[5px] rounded-full border px-[23.5px] py-[7px]">
+      <div className="flex items-center justify-center gap-[12px]">
+        <h1 className="text-[38px] font-bold">농구</h1>
+        <div className="relative">
+          <button
+            type="button"
+            onClick={() => {
+              setIsDropdownOpen(true)
+            }}
+          >
             <Image
-              src="/image/icon/icon-pencil.svg"
-              alt="pencil"
-              width={15}
-              height={15}
+              width={32}
+              height={32}
+              src="/image/icon/icon-menu.svg"
+              alt="menu"
             />
-            폴더편집
-          </div>
-          <div className="cursor-pointer">아이템 담으러가기</div>
+          </button>
+          {isDropdownOpen && (
+            <div
+              ref={dropdownRef}
+              className="absolute left-0 top-[40px] h-[157px] w-[173px] rounded-[8px] bg-white p-[13px_6px] shadow-[0px_0px_5.85px_2.25px_rgba(0,0,0,0.10)]"
+            >
+              <div className="mb-[10px] pl-[7px] text-[12px]">폴더 옵션</div>
+              <ul className="text-[14px] font-medium">
+                <li className="cursor-pointer rounded-[4px] p-[8px_7px] hover:bg-[#DFDFDF]">
+                  이름 수정
+                </li>
+                <li className="cursor-pointer rounded-[4px] p-[8px_7px] hover:bg-[#DFDFDF]">
+                  목록 편집
+                </li>
+                <li className="cursor-pointer rounded-[4px] p-[8px_7px] hover:bg-[#DFDFDF]">
+                  폴더 삭제
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
-      <div className="my-[40px]">
-        <div className="mx-auto grid max-w-[790px] grid-cols-[repeat(auto-fill,150px)] justify-center gap-x-[10px] gap-y-[25px]">
+      <div className="my-[80px]">
+        <div className="mx-auto grid max-w-[1200px] grid-cols-[repeat(auto-fill,150px)] justify-center gap-x-[10px] gap-y-[25px]">
+          <Item />
+          <Item />
+          <Item />
           <Item />
           <Item />
           <Item />
