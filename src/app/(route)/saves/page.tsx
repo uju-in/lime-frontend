1ecdac5e29073604b0ca4bdd82a58f2953894b87
yerim <pppp5097@naver.com>
2024-02-06 18:29:10 +0900

    design: 찜 목록 UI 수정사항 반영

--- src/app/(route)/saves/page.tsx
+++ src/app/(route)/saves/page.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image'
 import Layout from '@/app/_components/layout/Layout'
 import { Item } from '../items/_components/ItemList'
 import AddFolderModal from './_component/AddFolderModal'
+import { MoveFolderModal, SaveFolderItemList } from './[category]/_component'
+import { SavePageMode } from './[category]/_component/SaveFolderHeader'
 
 function SaveFolderItem({
   folderName,
@@ -37,55 +39,113 @@ function SaveFolderItem({
 
 export default function SavesPage() {
   const [showAddFolderModal, setShowAddFolderModal] = useState(false)
+  const [showMoveFolderModal, setShowMoveFolderModal] = useState(false)
+
   const [isEditMode, setIsEditMode] = useState(false) // 목록 편집 모드
+
+  const [checkedList, setCheckedList] = useState<number[]>([])
 
   return (
     <Layout>
-      <section className="h-full px-[150px]">
-        <div className="mb-[56px] flex items-center justify-between">
-          <h1 className="text-[30px] font-[700]">찜목록</h1>
-          <div className="flex gap-[16px]">
-            <button
-              className="flex items-center justify-center gap-[5.25px] rounded-[97.6px] border-[0.5px] border-[#E2E2E2] bg-white p-[10px_17px_10px_10px]"
-              type="button"
-              onClick={() => setIsEditMode(true)}
-            >
-              <Image
-                className="ml-1 cursor-pointer"
-                width={20}
-                height={20}
-                src="/image/icon/icon-pencil.svg"
-                alt="plus"
-              />
-              <p>목록 편집</p>
-            </button>
-            <button
-              className="flex items-center justify-center gap-[5.25px] rounded-[97.6px] bg-[#242424] p-[10px_17px_10px_10px]"
-              type="button"
-              onClick={() => setShowAddFolderModal(true)}
-            >
-              <Image
-                className="ml-1 cursor-pointer"
-                width={20}
-                height={20}
-                src="/image/icon/icon-white_plus.svg"
-                alt="plus"
-              />
-              <p className="text-[14px] font-[600] text-white">폴더 추가하기</p>
-            </button>
+      <section className="mx-auto h-full max-w-[1200px]">
+        {/* Header */}
+        {isEditMode ? (
+          <div className="relative mb-[56px]">
+            <h1 className="text-center text-[30px] font-bold">목록 편집</h1>
+            <section className="absolute top-[50px] flex w-full items-center justify-between">
+              <div className="text-[14px]">{`아이템 ${checkedList.length}개 선택됨`}</div>
+              <button
+                type="button"
+                className="rounded-full bg-[#b1b1b1] p-[9px_19px] text-white"
+              >
+                모두 선택
+              </button>
+            </section>
           </div>
-        </div>
+        ) : (
+          <div className="mb-[56px] flex items-center justify-between">
+            <h1 className="text-[30px] font-[700]">찜목록</h1>
+            <div className="flex gap-[16px]">
+              <button
+                className="flex items-center justify-center gap-[5.25px] rounded-[97.6px] border-[0.5px] border-[#E2E2E2] bg-white p-[10px_17px_10px_10px]"
+                type="button"
+                onClick={() => setIsEditMode(true)}
+              >
+                <Image
+                  className="ml-1 cursor-pointer"
+                  width={20}
+                  height={20}
+                  src="/image/icon/icon-pencil.svg"
+                  alt="plus"
+                />
+                <p>목록 편집</p>
+              </button>
+              <button
+                className="flex items-center justify-center gap-[5.25px] rounded-[97.6px] bg-[#242424] p-[10px_17px_10px_10px]"
+                type="button"
+                onClick={() => setShowAddFolderModal(true)}
+              >
+                <Image
+                  className="ml-1 cursor-pointer"
+                  width={20}
+                  height={20}
+                  src="/image/icon/icon-white_plus.svg"
+                  alt="plus"
+                />
+                <p className="text-[14px] font-[600] text-white">
+                  폴더 추가하기
+                </p>
+              </button>
+            </div>
+          </div>
+        )}
+
+        {/* Folder Items */}
         <div className="mb-[40px] grid grid-cols-[repeat(auto-fill,387px)] gap-[17px]">
           <SaveFolderItem folderName="농린이템" disabled={isEditMode} />
           <SaveFolderItem folderName="드로잉 준비물" disabled={isEditMode} />
           <SaveFolderItem folderName="배드민턴 라켓" disabled={isEditMode} />
         </div>
-        <div className="flex gap-[19px]">
-          <Item />
-          <Item />
-          <Item />
-        </div>
+
+        {/* Single Items */}
+        <SaveFolderItemList
+          mode={isEditMode ? SavePageMode.EDIT_LIST : SavePageMode.DEFAULT}
+          checkedList={checkedList}
+          setCheckedList={setCheckedList}
+        />
+
+        {/* Footer */}
+        {isEditMode && (
+          <section className="flex justify-center gap-6 py-[30px]">
+            <button type="button" onClick={() => setShowMoveFolderModal(true)}>
+              <Image
+                className="rounded-full bg-white p-[14px] shadow-[0px_0px_4.758px_rgba(0,0,0,0.10)]"
+                width={61}
+                height={61}
+                src="/image/icon/icon-folder.svg"
+                alt="folder"
+              />
+            </button>
+            <button type="button">
+              <Image
+                className="rounded-full bg-white p-[14px] shadow-[0px_0px_4.758px_rgba(0,0,0,0.10)]"
+                width={61}
+                height={61}
+                src="/image/icon/icon-bin.svg"
+                alt="bin"
+              />
+            </button>
+          </section>
+        )}
       </section>
+
+      {/* ------ 모달 ------ */}
+      {showMoveFolderModal && (
+        <MoveFolderModal
+          setShowMoveFolderModal={setShowMoveFolderModal}
+          setShowAddFolderModal={setShowAddFolderModal}
+        />
+      )}
       {showAddFolderModal && (
         <AddFolderModal setShowAddFolderModal={setShowAddFolderModal} />
       )}
