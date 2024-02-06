f5f9cb3584a8df8c5c51c78c6196c727cf709159
yerim <pppp5097@naver.com>
2024-02-06 18:00:18 +0900

    design: 찜 목록 폴더 css 피그마와 동일하게 수정

--- src/app/(route)/saves/page.tsx
+++ src/app/(route)/saves/page.tsx
@@ -7,8 +7,37 @@ import Layout from '@/app/_components/layout/Layout'
 import { Item } from '../items/_components/ItemList'
 import AddFolderModal from './_component/AddFolderModal'
 
+function SaveFolderItem({
+  folderName,
+  disabled,
+}: {
+  folderName: string
+  disabled: boolean
+}) {
+  return (
+    <div
+      className={`relative mr-5 flex h-[232px] w-[387px] ${disabled ? 'cursor-auto' : 'cursor-pointer'}`}
+    >
+      <div className="z-0 h-[232px] w-[240px] rounded-l-[8.83px] bg-[#BCBCBC]" />
+      <div className="z-0">
+        <div className="h-[116px] w-[147px] rounded-tr-[8.83px] bg-[#DBDBDB]" />
+        <div className="h-[116px] w-[147px] rounded-br-[8.83px] bg-[#F5F5F5]" />
+      </div>
+      {disabled && (
+        <div className="absolute left-0 top-0 z-20 h-full w-full bg-white opacity-80" />
+      )}
+      <div className="bg-gradient-folder absolute left-0 top-0 z-10 h-[193px] w-full rounded-t-[8.83px] pl-4 pt-4">
+        <p className="text-[20px] font-[700] text-white drop-shadow-[0.774px_0.774px_2.012px_rgba(0,0,0,0.30)]">
+          {folderName}
+        </p>
+      </div>
+    </div>
+  )
+}
+
 export default function SavesPage() {
   const [showAddFolderModal, setShowAddFolderModal] = useState(false)
+  const [isEditMode, setIsEditMode] = useState(false) // 목록 편집 모드
 
   return (
     <Layout>
@@ -19,6 +48,7 @@ export default function SavesPage() {
             <button
               className="flex items-center justify-center gap-[5.25px] rounded-[97.6px] border-[0.5px] border-[#E2E2E2] bg-white p-[10px_17px_10px_10px]"
               type="button"
+              onClick={() => setIsEditMode(true)}
             >
               <Image
                 className="ml-1 cursor-pointer"
@@ -45,39 +75,10 @@ export default function SavesPage() {
             </button>
           </div>
         </div>
-        <div className="mb-[40px] flex">
-          <div className="relative mr-5 flex h-[232px] w-[350px]">
-            <div className="z-0 h-[232px] w-[270px] rounded-l-[8.83px] bg-[#D2D2D2]" />
-            <div className="z-0">
-              <div className="h-[116px] w-[120px] rounded-tr-[8.83px] bg-[#E8E8E8]" />
-              <div className="h-[116px] w-[120px] rounded-br-[8.83px] bg-[#E8E8E8]" />
-            </div>
-            <div className="absolute left-0 top-0 z-10 h-[116px] w-full rounded-t-[8.83px] bg-gradient-to-b from-[#000] to-[#D2D2D2] pl-4 pt-4 opacity-50">
-              <p className="text-[20px] font-[700] text-white">농구</p>
-            </div>
-          </div>
-          <div className="relative mr-5 flex h-[232px] w-[350px]">
-            <div className="z-0 h-[232px] w-[270px] rounded-l-[8.83px] bg-[#D2D2D2]" />
-            <div className="z-0">
-              <div className="h-[116px] w-[120px] rounded-tr-[8.83px] bg-[#E8E8E8]" />
-              <div className="h-[116px] w-[120px] rounded-br-[8.83px] bg-[#E8E8E8]" />
-            </div>
-            <div className="absolute left-0 top-0 z-10 h-[116px] w-full rounded-t-[8.83px] bg-gradient-to-b from-[#000] to-[#D2D2D2] pl-4 pt-4 opacity-50">
-              <p className="text-[20px] font-[700] text-white">드로잉</p>
-            </div>
-          </div>
-          <div className="relative flex h-[232px] w-[350px]">
-            <div className="z-0 h-[232px] w-[270px] rounded-l-[8.83px] bg-[#D2D2D2]" />
-            <div className="z-0">
-              <div className="h-[116px] w-[120px] rounded-tr-[8.83px] bg-[#E8E8E8]" />
-              <div className="h-[116px] w-[120px] rounded-br-[8.83px] bg-[#E8E8E8]" />
-            </div>
-            <div className="absolute left-0 top-0 z-10 h-[116px] w-full rounded-t-[8.83px] bg-gradient-to-b from-[#000] to-[#D2D2D2]  opacity-50">
-              <p className="pl-4 pt-4 text-[20px] font-[700] text-white">
-                배드민턴
-              </p>
-            </div>
-          </div>
+        <div className="mb-[40px] grid grid-cols-[repeat(auto-fill,387px)] gap-[17px]">
+          <SaveFolderItem folderName="농린이템" disabled={isEditMode} />
+          <SaveFolderItem folderName="드로잉 준비물" disabled={isEditMode} />
+          <SaveFolderItem folderName="배드민턴 라켓" disabled={isEditMode} />
         </div>
         <div className="flex gap-[19px]">
           <Item />
