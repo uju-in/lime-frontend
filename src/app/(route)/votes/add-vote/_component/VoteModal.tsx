29fb0f1546650349f17fec077899bf5a19126869
mintmin <mintmin0320@gmail.com>
2024-02-12 19:18:36 +0900

    feat: 폴더 선택 로직 추가

--- src/app/(route)/votes/add-vote/_component/VoteModal.tsx
+++ src/app/(route)/votes/add-vote/_component/VoteModal.tsx
@@ -8,8 +8,10 @@ import Modal from '@/app/_components/modal'
 
 import { useSaveListData } from '@/app/_hook/api/useSaveListData'
 
-import { MemberItemMetadata } from '@/app/_types/saveItem.type'
+import { SaveItemListType } from '@/app/_types/saveItem.type'
+
 import ItemCard from './ItemCard'
+import FolderList from './FolderList'
 
 interface PropsType {
   setShowVoteModal: React.Dispatch<React.SetStateAction<boolean>>
@@ -21,26 +23,21 @@ export default function VoteModal(props: PropsType) {
 
   const router = useRouter()
 
-  /** 아이템 id 임시 선택 */
+  /** 아이템 id 선택 (미확정) */
   const [currentSelectedItem, setCurrentSelectedItem] = useState<number | null>(
     null,
   )
+  /** 폴더(id) 선택 */
+  const [folderId, setFolderId] = useState<number | null>(null)
   /** 아이템 선택 여부 */
   const [isSelected, setIsSelected] = useState<boolean>(false)
 
-  const { data, isLoading, isError } = useSaveListData()
+  const { data, isLoading, isError, isSuccess } = useSaveListData()
+
+  const { favoriteInfos, totalCount } = (data as SaveItemListType) ?? {}
 
   if (isLoading) return <div>Loading..</div>
   if (isError) return <div>Error !</div>
-
-  const { totalCount, favoriteInfos } = data ?? {}
-
-  /** 폴더 목록 저장 */
-  const folderList = data?.favoriteInfos.filter(
-    (item) => item.type === 'FOLDER',
-  )
-
-  console.log(folderList)
 
   const handleSelectItem = () => {
     if (!currentSelectedItem) {
@@ -51,57 +48,46 @@ export default function VoteModal(props: PropsType) {
     setShowVoteModal(false)
   }
 
-  return (
-    <Modal isScrollActive={false}>
-      <div className="h-[592px] w-[852px]">
-        <div className="flex justify-between py-[20px] pl-[46px] pr-[18px]">
-          <strong className="text-[24px] font-[600]">찜목록</strong>
-          <button
-            type="button"
-            aria-label="close"
-            onClick={() => {
-              setShowVoteModal(false)
-            }}
-          >
-            <Image
-              width={24}
-              height={24}
-              src="/image/icon/icon-close.svg"
-              alt="close"
-            />
-          </button>
-        </div>
-        <article className="px-[46px]">
-          <div className="flex h-[410px] w-[760px] rounded-[8px] border  border-[#DADADA]">
-            {/** folder choice */}
-            <div className="flex-1 overflow-y-scroll">
-              {folderList?.map((folder) => (
-                <button
-                  key={folder.favoriteId}
-                  type="button"
-                  className="flex w-full items-center border border-x-0 border-t-0 border-b-[#D2D2D2] py-[12px] pl-[18px]"
-                >
-                  <Image
-                    width={52}
-                    height={52}
-                    src="/image/icon/icon-close.svg"
-                    alt="folder image"
-                    className="rounded-[4px]"
-                  />
-                  <strong className="font=[500] ml-[16px]">농린이템</strong>
-                </button>
-              ))}
-            </div>
-            {/** 아이템 선택 */}
-            <div className="flex-1 overflow-y-auto pl-[16px]">
-              {totalCount !== 0 ? (
-                <>
-                  <p className="my-[13px] text-[12px]">{`아이템 ${totalCount}개`}</p>
-                  <div className="grid grid-cols-3 gap-x-[10px] gap-y-[20px]">
-                    {favoriteInfos
+  console.log(favoriteInfos)
+
+  if (isSuccess) {
+    return (
+      <Modal isScrollActive={false}>
+        <div className="h-[592px] w-[852px]">
+          <div className="flex justify-between py-[20px] pl-[46px] pr-[18px]">
+            <strong className="text-[24px] font-[600]">찜목록</strong>
+            <button
+              type="button"
+              aria-label="close"
+              onClick={() => {
+                setShowVoteModal(false)
+              }}
+            >
+              <Image
+                width={24}
+                height={24}
+                src="/image/icon/icon-close.svg"
+                alt="close"
+              />
+            </button>
+          </div>
+          <article className="px-[46px]">
+            <div className="flex h-[410px] w-[760px] rounded-[8px] border  border-[#DADADA]">
+              {/** 폴더 선택 */}
+              <FolderList
+                favoriteInfos={favoriteInfos}
+                folderId={folderId}
+                setFolderId={setFolderId}
+              />
+              {/** 아이템 선택 */}
+              <div className="flex-1 overflow-y-auto pl-[16px]">
+                {totalCount !== 0 ? (
+                  <>
+                    <p className="my-[13px] text-[12px]">{`아이템 ${totalCount}개`}</p>
+                    <div className="grid grid-cols-3 gap-x-[10px] gap-y-[20px]">
+                      {/* {favoriteInfos
                       ?.filter((item) => item.type === 'ITEM')
                       .map((item) => {
-                        // 여기서 구조 분해 할당을 사용합니다.
                         const { metadata } = item
                         const { memberItemMetadata } =
                           metadata as MemberItemMetadata
@@ -117,54 +103,55 @@ export default function VoteModal(props: PropsType) {
                             setIsSelected={setIsSelected}
                           />
                         )
-                      })}
+                      })} */}
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex h-full flex-col items-center justify-center">
+                    <strong className="mb-[12px] text-[20px] font-[600]">
+                      찜한 아이템이 없어요
+                    </strong>
+                    <p className="text-[14px] font-[500]">
+                      마음에 드는 아이템을 담아보세요
+                    </p>
                   </div>
-                </>
-              ) : (
-                <div className="flex h-full flex-col items-center justify-center">
-                  <strong className="mb-[12px] text-[20px] font-[600]">
-                    찜한 아이템이 없어요
-                  </strong>
-                  <p className="text-[14px] font-[500]">
-                    마음에 드는 아이템을 담아보세요
-                  </p>
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="mt-[43px] flex items-center justify-between">
-            <button
-              type="button"
-              className="flex items-center"
-              onClick={() => router.push('/items')}
-            >
-              <Image
-                width={15}
-                height={15}
-                src="/image/icon/icon-plus.svg"
-                alt="plus item"
-              />
-              <span className="relative ml-[6px] font-[600]">
-                아이템 담으러 가기
-              </span>
-            </button>
-            {/** 아이템 담으러가기 상단 안내 팝업 메시지 */}
-            <div className="absolute bottom-[60px] left-[45px] flex flex-col items-center">
-              <div className=" h-[27px] w-[152px] rounded-[4px] bg-[#000] px-[17px] py-[7px] text-[10px] font-[600] text-[#fff]">
-                취미에 맞는 아이템이 없다면?
+                )}
               </div>
-              <div className="h-0 w-0 border-l-[7px] border-r-[7px] border-t-[7px] border-l-transparent border-r-transparent border-t-black" />
             </div>
-            <button
-              type="button"
-              className="h-[40px] w-[110px] rounded-[100px] bg-[#000] px-[27px] text-center font-[600] text-[#fff]"
-              onClick={handleSelectItem}
-            >
-              선택완료
-            </button>
-          </div>
-        </article>
-      </div>
-    </Modal>
-  )
+            <div className="mt-[43px] flex items-center justify-between">
+              <button
+                type="button"
+                className="flex items-center"
+                onClick={() => router.push('/items')}
+              >
+                <Image
+                  width={15}
+                  height={15}
+                  src="/image/icon/icon-plus.svg"
+                  alt="plus item"
+                />
+                <span className="relative ml-[6px] font-[600]">
+                  아이템 담으러 가기
+                </span>
+              </button>
+              {/** 아이템 담으러가기 상단 안내 팝업 메시지 */}
+              <div className="absolute bottom-[60px] left-[45px] flex flex-col items-center">
+                <div className=" h-[27px] w-[152px] rounded-[4px] bg-[#000] px-[17px] py-[7px] text-[10px] font-[600] text-[#fff]">
+                  취미에 맞는 아이템이 없다면?
+                </div>
+                <div className="h-0 w-0 border-l-[7px] border-r-[7px] border-t-[7px] border-l-transparent border-r-transparent border-t-black" />
+              </div>
+              <button
+                type="button"
+                className="h-[40px] w-[110px] rounded-[100px] bg-[#000] px-[27px] text-center font-[600] text-[#fff]"
+                onClick={handleSelectItem}
+              >
+                선택완료
+              </button>
+            </div>
+          </article>
+        </div>
+      </Modal>
+    )
+  }
 }
