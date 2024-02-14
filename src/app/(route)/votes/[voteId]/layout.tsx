aede375980bb6e1fcc84a04b6fbe7822505ed0ac
mintmin <mintmin0320@gmail.com>
2024-02-15 00:30:13 +0900

    refactor: 투표 Layout 공통 적용

--- src/app/(route)/votes/[voteId]/layout.tsx
+++ src/app/(route)/votes/[voteId]/layout.tsx
@@ -1,13 +1,7 @@
-import Layout from '@/app/_components/layout/Layout'
-
 export default function DetailItemLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <Layout>
-      <main className="flex h-dvh justify-center">{children}</main>
-    </Layout>
-  )
+  return <main className="flex h-dvh justify-center">{children}</main>
 }
--- src/app/(route)/votes/add-vote/layout.tsx
+++ src/app/(route)/votes/add-vote/layout.tsx
@@ -1,16 +1,7 @@
-import RQProvider from '@/app/_components/RQProvider'
-import Layout from '@/app/_components/layout/Layout'
-
 export default function AddVoteLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <Layout>
-      <RQProvider>
-        <main className="flex justify-center bg-[#F7F7F7]">{children}</main>
-      </RQProvider>
-    </Layout>
-  )
+  return <main className="flex justify-center bg-[#F7F7F7]">{children}</main>
 }
