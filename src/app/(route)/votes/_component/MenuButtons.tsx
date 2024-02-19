dec7d010baa3851e7acc143bfb0b9728bc0fd7ff
mintmin <mintmin0320@gmail.com>
2024-02-19 18:57:56 +0900

    refactor: 취미 선택 url 변경 로직 추가

--- src/app/(route)/votes/_component/MenuButtons.tsx
+++ src/app/(route)/votes/_component/MenuButtons.tsx
@@ -2,16 +2,16 @@
 
 import { CategoryOption } from '@/app/_constants'
 import useGetSearchParam from '@/app/_hook/common/useGetSearchParams'
+import { cn } from '@/app/_utils/twMerge'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { defaultCategory } from '../_utils/defaultCategory'
 
 export default function MenuButtons() {
   const router = useRouter()
 
-  // const title = useGetSearchParam('title')
-  const title = '스포츠'
-  // const category = useGetSearchParam('category')
-  const category = '농구'
+  const title = useGetSearchParam('title') || '스포츠'
+  const category = useGetSearchParam('category') || '농구'
 
   const categoryList = CategoryOption.find((item) => {
     return item.title === title
@@ -21,32 +21,44 @@ export default function MenuButtons() {
     <>
       <ul className="flex gap-[15px] text-[20px] font-[700]">
         {CategoryOption.map((item) => (
-          <li
+          <Link
+            href={`/votes?title=${item.title}&category=${defaultCategory(
+              item.title,
+            )}`}
             key={item.title}
-            className="h-[42.5px] w-[86.5px] rounded-[83.333px] bg-[#000] px-[16px] py-[8px] text-[#fff]"
           >
-            {item.title}
-          </li>
+            <li
+              className={cn(
+                'h-[42.5px] w-[86.5px] cursor-pointer rounded-[83.333px] px-[16px] py-[8px] text-[#fff]',
+                {
+                  'bg-black text-white': title === item.title,
+                  'bg-white text-[#AAA]': title !== item.title,
+                },
+              )}
+            >
+              {item.title}
+            </li>
+          </Link>
         ))}
       </ul>
-      <ul className="mt-[20px] flex gap-[15px]">
-        {/* <button
-          type="button"
-          className="h-[39px] w-[60px] border-[3px] border-x-0 border-t-0 border-b-[#000] font-[600]"
-        >
-          농구
-        </button> */}
+      <ul className="mt-[20px] flex gap-[15px] text-center">
         {categoryList &&
           categoryList.list.map((item) => {
             return (
-              <li
-                key={item}
-                className="h-[39px] w-[60px] border-[3px] border-x-0 border-t-0 border-b-[#000] font-[600]"
-              >
-                <Link href={`/items?title=${title}&category=${item}`}>
+              <Link href={`/votes?title=${title}&category=${item}`} key={item}>
+                <li
+                  className={cn(
+                    'h-[39px] min-w-[60px] cursor-pointer  border-[3px] font-[600]',
+                    {
+                      'border-x-0 border-t-0 border-b-[#000]':
+                        category === item,
+                      'border-0': category !== item,
+                    },
+                  )}
+                >
                   {item}
-                </Link>
-              </li>
+                </li>
+              </Link>
             )
           })}
       </ul>
