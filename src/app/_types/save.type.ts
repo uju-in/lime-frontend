a61a12aaf5f7a69ef7e27c908f5f557187192799
yerim <pppp5097@naver.com>
2024-02-20 18:23:19 +0900

    feat: 찜 아이템, 폴더 타입 추가

--- src/app/_types/save.type.ts
+++ src/app/_types/save.type.ts
@@ -3,3 +3,30 @@ export const enum SavePageMode {
   CHANGE_NAME = 1,
   EDIT_LIST = 2,
 }
+
+export interface SaveItemType {
+  favoriteId: number
+  originalName: string
+  type: 'ITEM' | 'FOLDER'
+  metadata: {
+    favoriteItemMetadata: {
+      imageUrl: string
+      itemId: number
+      price: number
+      favoriteCount: number
+      reviewCount: number
+    }
+  }
+}
+
+export interface SaveFolderType {
+  favoriteId: number
+  originalName: string
+  type: 'ITEM' | 'FOLDER'
+  metadata: {
+    folderMetadata: {
+      imageUrls: string[]
+      itemCount: number
+    }
+  }
+}
