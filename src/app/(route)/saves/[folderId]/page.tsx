9e3b863ccf39708d1ae8cef978335ae104d46470
yerim <pppp5097@naver.com>
2024-02-26 20:54:58 +0900

    style: 컴포넌트 분리

--- src/app/(route)/saves/[folderId]/page.tsx
+++ src/app/(route)/saves/[folderId]/page.tsx
@@ -1,15 +1,12 @@
 'use client'
 
 import React, { useState } from 'react'
-import Image from 'next/image'
 import Layout from '@/app/_components/layout/Layout'
-import { SavePageMode } from '@/app/_types/save.type'
 import RQProvider from '@/app/_components/RQProvider'
 
 import AddFolderModal from '../_component/AddFolderModal'
-import SaveItemList from './_component/SaveItemList'
 import MoveFolderModal from '../_component/MoveFolderModal'
-import { SaveFolderHeader } from './_component'
+import SaveComponent from './_component/SaveComponent'
 
 type Props = {
   params: { folderId: number }
@@ -20,72 +17,14 @@ export default function SavesDetailPage({ params }: Props) {
   const [showMoveFolderModal, setShowMoveFolderModal] = useState(false)
   const [showAddFolderModal, setShowAddFolderModal] = useState(false)
 
-  const [mode, setMode] = useState<SavePageMode>(SavePageMode.DEFAULT)
-  const [checkedList, setCheckedList] = useState<number[]>([])
-
   return (
     <Layout>
-      <div className="mx-auto max-w-[1200px]">
-        {/* Header */}
-        <RQProvider>
-          <section className="relative mb-[100px] flex items-center justify-center gap-[12px]">
-            {mode === SavePageMode.DEFAULT && (
-              <SaveFolderHeader.Default
-                setMode={setMode}
-                folderId={Number(folderId)}
-              />
-            )}
-            {mode === SavePageMode.CHANGE_NAME && (
-              <SaveFolderHeader.ChangeName
-                setMode={setMode}
-                folderId={Number(folderId)}
-              />
-            )}
-            {mode === SavePageMode.EDIT_LIST && (
-              <SaveFolderHeader.EditList checkedList={checkedList} />
-            )}
-          </section>
-        </RQProvider>
-
-        {/* Contents */}
-        <SaveItemList
-          mode={mode}
-          checkedList={checkedList}
-          setCheckedList={setCheckedList}
+      <RQProvider>
+        <SaveComponent
+          folderId={Number(folderId)}
+          setShowMoveFolderModal={setShowMoveFolderModal}
         />
-
-        {/* Footer */}
-        {mode === SavePageMode.EDIT_LIST && (
-          <section className="flex justify-center gap-6 py-[30px]">
-            <button type="button" onClick={() => setShowMoveFolderModal(true)}>
-              <Image
-                className="rounded-full bg-white p-[14px] shadow-[0px_0px_4.758px_rgba(0,0,0,0.10)]"
-                width={61}
-                height={61}
-                src="/image/icon/icon-folder.svg"
-                alt="folder"
-              />
-            </button>
-            <button type="button">
-              <Image
-                className="rounded-full bg-white p-[14px] shadow-[0px_0px_4.758px_rgba(0,0,0,0.10)]"
-                width={61}
-                height={61}
-                src="/image/icon/icon-bin.svg"
-                alt="bin"
-              />
-            </button>
-            <button type="button" onClick={() => setMode(SavePageMode.DEFAULT)}>
-              <Image
-                className="rounded-full bg-white p-[14px] shadow-[0px_0px_4.758px_rgba(0,0,0,0.10)]"
-                width={61}
-                height={61}
-                src="/image/icon/icon-cancel.svg"
-                alt="cancel"
-              />
-            </button>
-          </section>
-        )}
+        {/* // ----- 모달 ----- // */}
         {showMoveFolderModal && (
           <MoveFolderModal
             setShowMoveFolderModal={setShowMoveFolderModal}
@@ -95,7 +34,7 @@ export default function SavesDetailPage({ params }: Props) {
         {showAddFolderModal && (
           <AddFolderModal setShowAddFolderModal={setShowAddFolderModal} />
         )}
-      </div>
+      </RQProvider>
     </Layout>
   )
 }
