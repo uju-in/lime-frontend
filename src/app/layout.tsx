16f7a1d56256b53061e8b37a68b75f8d7d989276
mintmin <mintmin0320@gmail.com>
2024-02-28 18:23:05 +0900

    style: 변경 언어 설정을 한국어로 (lang="ko")

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={inter.className}>
         <Toaster />
         {children}
