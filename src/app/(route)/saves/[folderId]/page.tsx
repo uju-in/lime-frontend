3882e4ac64647bd9ead76124072d7065ac4a6b2f
yerim <pppp5097@naver.com>
2024-02-28 20:36:54 +0900

    style: 불필요한 코드 삭제

--- src/app/(route)/saves/[folderId]/page.tsx
+++ src/app/(route)/saves/[folderId]/page.tsx
@@ -1,9 +1,6 @@
-'use client'
-
 import React from 'react'
 import Layout from '@/app/_components/layout/Layout'
 import RQProvider from '@/app/_components/RQProvider'
-import useGetSearchParam from '@/app/_hook/common/useGetSearchParams'
 
 import SaveFolderComponent from './_component/SaveFolderComponent'
 
