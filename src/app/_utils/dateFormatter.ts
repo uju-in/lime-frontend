8a8db5f7c722ab90dd9e846bffb4bc2e47284bc2
mintmin <mintmin0320@gmail.com>
2024-02-29 15:29:35 +0900

    feat: 날짜 변환기 시, 분 추가

--- src/app/_utils/dateFormatter.ts
+++ src/app/_utils/dateFormatter.ts
@@ -11,3 +11,22 @@ export function dateFormatter(dateString: string): string {
     .replace(/(\d{4})\. (\d{2})\. (\d{2})\./, '$1.$2.$3')
     .trim()
 }
+
+export function detailDateFormatter(dateString: string): string {
+  const date = new Date(dateString)
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  }
+
+  const formattedDate = new Intl.DateTimeFormat('ko-KR', options).format(date)
+
+  return formattedDate
+    .replace(/(\d{4})\. (\d{2})\. (\d{2})\. (\d{2}):(\d{2})/, '$1.$2.$3.$4.$5')
+    .trim()
+}
