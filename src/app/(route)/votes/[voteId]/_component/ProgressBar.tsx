7b8fa480e9c163f48b49b66b2c464cb6c2f6d26c
mintmin <mintmin0320@gmail.com>
2024-02-29 20:51:59 +0900

    feat: 투표 0명 참여 상태바 UI 추가

--- src/app/(route)/votes/[voteId]/_component/ProgressBar.tsx
+++ src/app/(route)/votes/[voteId]/_component/ProgressBar.tsx
@@ -10,34 +10,49 @@ interface PropsType {
 export default function ProgressBar({ item1Votes, item2Votes }: PropsType) {
   const maxWidth = 514
   const totalVotes = item1Votes + item2Votes
-  const item1Width =
-    totalVotes > 0 ? (maxWidth * item1Votes) / totalVotes : maxWidth / 2
-  const item2Width =
-    totalVotes > 0 ? (maxWidth * item2Votes) / totalVotes : maxWidth / 2
+  const item1Width = (maxWidth * item1Votes) / totalVotes
+  const item2Width = (maxWidth * item2Votes) / totalVotes
+
+  if (item1Votes === 0 && item2Votes === 0) {
+    return (
+      <div className="mt-[26px] flex h-[49px] w-[514px] items-center text-[22px] font-[500]">
+        <div className="flex h-full w-full items-center justify-center rounded-[8px] bg-[#000] text-[#fff]">
+          0명
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="mt-[26px] flex h-[49px] items-center text-[22px] font-[500] ">
-      <div
-        style={{ width: `${item1Width}px` }}
-        className={cn(
-          'flex h-full items-center justify-center bg-[#000] text-[#fff]',
-          {
-            'rounded-[8px]': item2Votes === 0,
-            'rounded-l-[8px]': item2Votes !== 0,
-          },
-        )}
-      >
-        {item1Votes}명
-      </div>
-      <div
-        style={{ width: `${item2Width}px` }}
-        className={cn('flex h-full items-center justify-center bg-[#EAEAEA]', {
-          'rounded-[8px]': item1Votes === 0,
-          'rounded-r-[8px]': item1Votes !== 0,
-        })}
-      >
-        {item2Votes}명
-      </div>
+      {item1Votes !== 0 && (
+        <div
+          style={{ width: `${item1Width}px` }}
+          className={cn(
+            'flex h-full items-center justify-center bg-[#000] text-[#fff]',
+            {
+              'rounded-[8px]': item2Votes === 0,
+              'rounded-l-[8px]': item2Votes !== 0,
+            },
+          )}
+        >
+          {item1Votes}명
+        </div>
+      )}
+      {item2Votes !== 0 && (
+        <div
+          style={{ width: `${item2Width}px` }}
+          className={cn(
+            'flex h-full items-center justify-center bg-[#EAEAEA]',
+            {
+              'rounded-[8px]': item1Votes === 0,
+              'rounded-r-[8px]': item1Votes !== 0,
+            },
+          )}
+        >
+          {item2Votes}명
+        </div>
+      )}
     </div>
   )
 }
