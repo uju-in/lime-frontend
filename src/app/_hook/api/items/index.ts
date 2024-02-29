2d8e1cc3daeedb64d1b183b2852f07030d76919b
yerim <pppp5097@naver.com>
2024-02-29 23:37:32 +0900

    feat: 검색 API 추가

--- src/app/_hook/api/items/index.ts
+++ src/app/_hook/api/items/index.ts
@@ -2,6 +2,7 @@ import { createQueryKeys } from '@lukemorales/query-key-factory'
 
 export const itemKeys = createQueryKeys('items', {
   itemList: (keyword: string, sortOption: string) => [keyword, sortOption],
+  searchList: (keyword: string) => [keyword],
 })
 
 export const itemTags = {
