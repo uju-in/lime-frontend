ff95ab7c5ec1ddd6cffb2715b2bd563f9c6ae58c
mintmin <mintmin0320@gmail.com>
2024-02-29 15:43:14 +0900

    style: 메뉴 버튼 클릭 이벤트 위치 수정

--- src/app/(route)/votes/_component/MenuButtons.tsx
+++ src/app/(route)/votes/_component/MenuButtons.tsx
@@ -71,13 +71,13 @@ export default function MenuButtons() {
       <button
         type="button"
         className="flex h-[73px] w-[64px] flex-col items-center justify-center gap-[4px] rounded-[8px] border-2 border-black"
+        onClick={() => router.push('/votes/add-vote')}
       >
         <Image
           width={24}
           height={24}
           src="/image/icon/icon-plus.svg"
           alt="add vote"
-          onClick={() => router.push('/votes/add-vote')}
         />
         <span className="text-[12px] font-[600]">투표생성</span>
       </button>
