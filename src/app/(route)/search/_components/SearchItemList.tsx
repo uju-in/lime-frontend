5ad0e7174162a705607dfe89a10e6de1e464a45c
yerim <pppp5097@naver.com>
2024-03-04 03:02:57 +0900

    design: 검색 결과 없을 때 UI 추가

--- src/app/(route)/search/_components/SearchItemList.tsx
+++ src/app/(route)/search/_components/SearchItemList.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 import { ItemType } from '@/app/_types/item.type'
 import useItemListData from '@/app/_hook/api/items/useItemListData'
-import useGetSearchParam from '@/app/_hook/common/useGetSearchParams'
 import { useInView } from 'react-intersection-observer'
 import { SortOption } from '../_constants'
 import SortBox from './SortBox'
@@ -79,9 +79,32 @@ export default function SearchItemList({ keyword }: { keyword: string }) {
   if (isLoading) return <div>Loading..</div>
   if (isError) return null
 
+  if (!itemList.length)
+    return (
+      <div className="my-[136px] flex flex-col gap-[30px] text-center">
+        <p className="text-[20px]">아이템 검색 결과가 없어요.</p>
+        <Link
+          href="/items/add-item"
+          className="flex items-center justify-center gap-[6px] text-[16px] text-[#858585]"
+        >
+          <Image
+            className="mb-[2px]"
+            src="/image/icon/icon-plus_858585.svg"
+            alt="plus"
+            width={12}
+            height={12}
+          />
+          <span className="text-[16px] font-semibold">아이템 생성하러가기</span>
+        </Link>
+      </div>
+    )
+
   return (
     <>
-      <div className="relative my-[30px] flex items-center justify-end">
+      <div className="relative my-[30px] flex items-center justify-between">
+        <p className="text-[14px] font-medium">
+          총 {itemList.length}개의 아이템
+        </p>
         <SortBox sortOption={sortOption} setSortOption={setSortOption} />
       </div>
       <div className="grid grid-cols-[repeat(auto-fill,184px)] gap-x-[19px] gap-y-[25px]">
