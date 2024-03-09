cd9e4e9aea975a248ee47af546387585c02b899f
mintmin <mintmin0320@gmail.com>
2024-03-09 13:16:18 +0900

    feat: 모바일 검색바 구현

--- src/app/(route)/(withLayout)/votes/page.tsx
+++ src/app/(route)/(withLayout)/votes/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 import MoNavbar from '@/app/_components/layout/mobile/MoNavbar'
 import { cn } from '@/app/_utils/twMerge'
 import MoAddButton from '@/app/_components/layout/mobile/MoAddButton'
+import MoSearchBar from '@/app/_components/layout/mobile/MoSearchBar'
 import CategoryPicker from '../../../_components/categoryPicker/CategoryPicker'
 import RankingList from './_component/RankingList'
 import VoteList from './_component/VoteList'
@@ -11,7 +12,8 @@ export default function page() {
   return (
     <>
       <main className={cn('mx-auto w-[794px]', 'mo:w-full')}>
-        <div className="mt-[52px] flex justify-between">
+        <MoSearchBar path="투표" />
+        <div className={cn('mt-[52px] flex justify-between', 'mo:mt-[21px]')}>
           <CategoryPicker path="votes" />
           <CreateVoteButton />
         </div>
