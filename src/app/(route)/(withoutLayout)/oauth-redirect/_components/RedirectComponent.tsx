57674774df9fb1fbfd3e67be03e4594f6c103c65
mintmin <mintmin0320@gmail.com>
2024-03-11 18:32:46 +0900

    refactor: 토큰 저장 로직 변경

--- src/app/(route)/(withoutLayout)/oauth-redirect/_components/RedirectComponent.tsx
+++ src/app/(route)/(withoutLayout)/oauth-redirect/_components/RedirectComponent.tsx
@@ -1,9 +1,14 @@
 'use client'
 
 import { useRequestToken } from '@/app/_hook/api/auth/useRequestToken'
+import { useEffect } from 'react'
 
 export default function RedirectComponent() {
-  useRequestToken()
+  const authLogin = useRequestToken()
+
+  useEffect(() => {
+    authLogin()
+  }, [])
 
   return null
 }
--- src/app/_hook/api/auth/useRequestToken.ts
+++ src/app/_hook/api/auth/useRequestToken.ts
@@ -52,7 +52,5 @@ export function useRequestToken() {
     }
   }
 
-  useEffect(() => {
-    authLogin()
-  }, [])
+  return authLogin
 }
