49ca1281ae7c79b19a3b4aa5e1e85af3333dd3d2
mintmin <mintmin0320@gmail.com>
2024-03-14 14:43:37 +0900

    design: 홈 아이템 리스트 반응형 적용

--- src/app/(route)/_components/ItemSection.tsx
+++ src/app/(route)/_components/ItemSection.tsx
@@ -1,4 +1,5 @@
 import { fetchItems } from '@/app/_hook/api/home/useGetItems'
+import { cn } from '@/app/_utils/twMerge'
 import Item from '../(withLayout)/items/_components/Item'
 
 export default async function ItemSection({
@@ -11,9 +12,19 @@ export default async function ItemSection({
   const { items } = itemData
 
   return (
-    <div className="mt-[32px] flex gap-[19px]">
+    <div
+      className={cn(
+        'mt-[32px] grid grid-cols-[repeat(auto-fill,184px)] gap-[17px]',
+        'mo:grid-cols-3',
+      )}
+    >
       {items.map((item) => (
-        <Item key={item.cursorId} item={item} />
+        <div
+          key={item.cursorId}
+          className={cn('h-[290px] w-[184px]', 'mo:w-full')}
+        >
+          <Item item={item} />
+        </div>
       ))}
     </div>
   )
