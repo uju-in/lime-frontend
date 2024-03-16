4ed8ee1fd4bd159a82fa19ae44b34036ab4cdb74
mintmin <mintmin0320@gmail.com>
2024-03-17 00:48:50 +0900

    design: 모달 모바일 반응형 추가

--- src/app/_components/modal/index.tsx
+++ src/app/_components/modal/index.tsx
@@ -13,17 +13,18 @@ function Modal({ isScrollActive = false, children }: ModalProps) {
     <div
       id="ModalContainer"
       className="fixed bottom-0 left-0 right-0 top-0 z-[500] h-screen min-h-screen w-full bg-[rgba(0,0,0,0.7)]"
-      onClick={(e) => e.stopPropagation()}
     >
       <div
         id="ModalInner"
         className={cn(
           'absolute left-[50%] top-[50%] max-h-[90vh] max-w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-[8px] bg-white',
+          'mo:bottom-0 mo:top-[15%] mo:w-full mo:max-w-full mo:-translate-y-0',
           {
             'overflow-y-scroll': isScrollActive,
             'overflow-x-hidden': !isScrollActive,
           },
         )}
+        onClick={(e) => e.stopPropagation()}
       >
         {children}
       </div>
