a4ddbffa6fd049e3b3eee50102fc8395234b21cf
mintmin <mintmin0320@gmail.com>
2024-03-17 23:34:31 +0900

    refactor: 투표 아이템 선택 영역 컴포넌트화

--- src/app/(route)/(withLayout)/votes/add-vote/_component/VoteItemList.tsx
+++ src/app/(route)/(withLayout)/votes/add-vote/_component/VoteItemList.tsx
@@ -7,10 +7,8 @@ import {
   SaveItemType,
 } from '@/app/_types/saveItem.type'
 import { cn } from '@/app/_utils/twMerge'
-import { useRecoilState } from 'recoil'
 import FolderList from './FolderList'
-import FavoriteList from './FavoriteItemList'
-import ItemsNotFound from './ItemsNotFound'
+import VoteItemsSelector from './VoteItemsSelector'
 
 interface PropsType {
   setCurrentSelectedItem: React.Dispatch<
@@ -75,28 +73,19 @@ export default function VoteItemList(props: PropsType) {
         </div>
       ) : (
         <div className={cn('hidden w-full', 'mo:block')}>
-          <FavoriteList
-            folderId={selectedFolder.folderId}
+          <VoteItemsSelector
+            selectedFolder={selectedFolder}
             currentSelectedItem={currentSelectedItem}
             setCurrentSelectedItem={setCurrentSelectedItem}
           />
         </div>
       )}
       <div className={cn('flex-1 overflow-y-auto pl-[16px]', 'mo:hidden')}>
-        {selectedFolder.itemCount !== 0 ? (
-          <>
-            <p className="my-[13px] text-[12px]">
-              아이템 {selectedFolder.itemCount}개
-            </p>
-            <FavoriteList
-              folderId={selectedFolder.folderId}
-              currentSelectedItem={currentSelectedItem}
-              setCurrentSelectedItem={setCurrentSelectedItem}
-            />
-          </>
-        ) : (
-          <ItemsNotFound />
-        )}
+        <VoteItemsSelector
+          selectedFolder={selectedFolder}
+          currentSelectedItem={currentSelectedItem}
+          setCurrentSelectedItem={setCurrentSelectedItem}
+        />
       </div>
     </div>
   )
