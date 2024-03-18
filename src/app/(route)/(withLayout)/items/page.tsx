def8611ddb3b644f049d26ed6fa09dc3749caffd
yerim <pppp5097@naver.com>
2024-03-19 03:49:50 +0900

    feat: 아이템 리스트 페이지 CategoryPicker 추가

--- src/app/(route)/(withLayout)/items/page.tsx
+++ src/app/(route)/(withLayout)/items/page.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { cn } from '@/app/_utils/twMerge'
+import CategoryPicker from '@/app/_components/categoryPicker/CategoryPicker'
 import SideMenu from './_components/SideMenu'
 import ItemList from './_components/ItemList'
 
 export default function page() {
   return (
-    <div
-      className={cn(
-        'mx-auto flex w-[1024px] max-w-full justify-center gap-[50px] px-[10px]',
-        'mo:px-[16px]',
-      )}
-    >
-      <SideMenu />
-      <div className="flex-1">
-        <ItemList />
+    <>
+      <div
+        className={cn('mt-[16px] hidden justify-between pt-[56px]', 'mo:flex')}
+      >
+        <CategoryPicker path="/items" />
       </div>
-    </div>
+      <div
+        className={cn(
+          'mx-auto flex w-[1024px] max-w-full justify-center gap-[50px] px-[10px]',
+          'mo:px-[16px]',
+        )}
+      >
+        <SideMenu />
+        <div className="flex-1">
+          <ItemList />
+        </div>
+      </div>
+    </>
   )
 }
