8ebb1f24c42cfdc183fd909e1f8c5f59d5528525
mintmin <mintmin0320@gmail.com>
2024-03-18 18:26:38 +0900

    design: 최상위 layout margin 값 조정

--- src/app/(route)/(withLayout)/layout.tsx
+++ src/app/(route)/(withLayout)/layout.tsx
@@ -6,7 +6,7 @@ export default function WithLayout({ children }: { children: ReactNode }) {
   return (
     <main className="my-0 flex flex-col">
       <Header />
-      <div className="pb-[38px]">{children}</div>
+      <div className="mb-[18px]">{children}</div>
       <Footer />
     </main>
   )
