d14fc9889a249a75ffe10b1a836f5a573494d6b5
yerim <pppp5097@naver.com>
2024-03-19 18:40:27 +0900

    fix: use client 키워드 위치 이동

--- src/app/(route)/(withLayout)/items/_components/FloatingButton.tsx
+++ src/app/(route)/(withLayout)/items/_components/FloatingButton.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { cn } from '@/app/_utils/twMerge'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
--- src/app/(route)/(withLayout)/items/layout.tsx
+++ src/app/(route)/(withLayout)/items/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { ReactNode } from 'react'
 import RQProvider from '@/app/_components/RQProvider'
 import MoNavbar from '@/app/_components/layout/mobile/MoNavbar'
