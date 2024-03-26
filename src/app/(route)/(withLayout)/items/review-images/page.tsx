748f1cc80589a19c558a5194925a921f3981b17b
mintmin <mintmin0320@gmail.com>
2024-03-27 00:27:39 +0900

    feat: 리뷰 상세 사진 페이지 추가

---
+++ src/app/(route)/(withLayout)/items/review-images/page.tsx
@@ -0,0 +1,11 @@
+import DetailImage from './_component/DetailImage'
+import MoImageDetailHeader from './_component/MoImageDetailHeader'
+
+export default function page() {
+  return (
+    <>
+      <MoImageDetailHeader />
+      <DetailImage />
+    </>
+  )
+}
