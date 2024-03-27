19bb05a6199660fed38e9d0fb74f7ed48cc105d0
yerim <pppp5097@naver.com>
2024-03-27 18:08:44 +0900

    refactor: 찜 목록 헤더 상세 페이지에서도 사용할 수 있도록 수정

--- src/app/(route)/(withLayout)/saves/_component/MoSavesHeader.tsx
+++ src/app/(route)/(withLayout)/saves/_component/MoSavesHeader.tsx
@@ -2,12 +2,16 @@
 
 import { MoHeader } from '@/app/_components/layout/mobile/MoHeader'
 import { cn } from '@/app/_utils/twMerge'
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
 export default function MoSavesHeader() {
+  const searchParams = useSearchParams()
+  const title = searchParams.get('name') || ''
+
   return (
     <div className={cn('hidden', 'mo:block')}>
-      <MoHeader.Save title="찜목록" />
+      <MoHeader.Save title={title} />
     </div>
   )
 }
