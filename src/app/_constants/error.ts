5c45e14a803d9c31a73726bc8c5d1b54434856ed
mintmin <mintmin0320@gmail.com>
2024-04-05 22:14:50 +0900

    feat: 에러 처리 핸들러 추가

---
+++ src/app/_constants/error.ts
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGE = {
+  PROFILE_COMPLETION_REQUIRED: '프로필 작성이 필요한 서비스입니다.',
+  LOGIN_REQUIRED: '로그인이 필요한 서비스입니다.',
+}
+
+export const ERROR_CODE = {
+  INVALID_ACCESS_TOKEN: 'COMMON_009',
+  INSUFFICIENT_PERMISSION: 'MEMBER_017',
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,58 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { fetchTokenValidity } from './app/_hook/api/auth/useTokenValidity'
+import { ERROR_CODE } from './app/_constants/error'
+
+interface ApiError {
+  code?: string
+  message: string
+}
 
 export async function middleware(request: NextRequest) {
-  const accessToken = request.cookies.get('accessToken')?.value ?? ''
-
-  const isValidToken = accessToken
-    ? await fetchTokenValidity(accessToken)
-    : false
-
   const url = new URL(request.url)
   const currentPath = url.pathname
+  const accessToken = request.cookies.get('accessToken')?.value ?? ''
 
-  const redirectToLogin =
-    ['/mypage', '/saves'].some((path) => currentPath.startsWith(path)) &&
+  // 접근 제한이 필요한 경로
+  const protectedPaths = [
+    '/mypage',
+    '/saves',
+    '/items/add-item',
+    '/votes/add-vote',
+  ]
+  const authPaths = ['/login', '/join']
+
+  // 현재 경로가 로그인이나 회원가입 경로에 있을 경우, 추가 처리 없이 요청을 계속 진행
+  if (authPaths.includes(currentPath)) {
+    return NextResponse.next()
+  }
+
+  let isValidToken = false
+
+  try {
+    isValidToken = accessToken ? await fetchTokenValidity(accessToken) : false
+
+    if (!isValidToken) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  } catch (error: unknown) {
+    if (
+      typeof error === 'object' &&
+      error !== null &&
+      'code' in error &&
+      (error as ApiError).code === ERROR_CODE.INSUFFICIENT_PERMISSION
+    ) {
+      return NextResponse.redirect(new URL('/join', request.url))
+    }
+  }
+
+  if (
+    protectedPaths.some((path) => currentPath.startsWith(path)) &&
     !isValidToken
-  const redirectToHome =
-    ['/login', '/join'].includes(currentPath) && isValidToken
-
-  if (redirectToLogin) {
+  ) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  if (redirectToHome) {
+  if (authPaths.includes(currentPath) && isValidToken) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
@@ -29,5 +60,13 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/mypage', '/login', '/join', '/saves', '/saves/:path*'],
+  matcher: [
+    '/mypage',
+    '/login',
+    '/join',
+    '/saves',
+    '/saves/:path*',
+    '/items/add-item',
+    '/votes/add-vote',
+  ],
 }
