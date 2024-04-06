d415e32b698d7df6354e8a63f9b5a8d1868b04e0
mintmin <mintmin0320@gmail.com>
2024-04-06 15:11:04 +0900

    refactor: if else 문 -> switch 문으로 전환

--- src/app/_hook/common/useHandleApiError.ts
+++ src/app/_hook/common/useHandleApiError.ts
@@ -11,19 +11,25 @@ export const useHandleApiError = () => {
   const router = useRouter()
 
   return (error: ApiError) => {
-    if (error.code === ERROR_CODE.INSUFFICIENT_PERMISSION) {
-      if (window.confirm(ERROR_MESSAGE.PROFILE_COMPLETION_REQUIRED)) {
-        router.push('/join')
-      }
-    } else if (error.code === ERROR_CODE.INVALID_ACCESS_TOKEN) {
-      if (window.confirm(ERROR_MESSAGE.LOGIN_REQUIRED)) {
-        router.push('/login')
-      }
-    } else {
-      renderToast({
-        type: 'error',
-        message: String(error.message),
-      })
+    switch (error.code) {
+      case ERROR_CODE.INSUFFICIENT_PERMISSION:
+        if (window.confirm(ERROR_MESSAGE.PROFILE_COMPLETION_REQUIRED)) {
+          router.push('/join')
+        }
+        break
+
+      case ERROR_CODE.INVALID_ACCESS_TOKEN:
+        if (window.confirm(ERROR_MESSAGE.LOGIN_REQUIRED)) {
+          router.push('/login')
+        }
+        break
+
+      default:
+        renderToast({
+          type: 'error',
+          message: String(error.message),
+        })
+        break
     }
   }
 }
