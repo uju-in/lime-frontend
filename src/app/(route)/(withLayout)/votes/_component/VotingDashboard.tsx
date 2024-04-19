b297d994d5972ce838433e16b8f860a1bd19caae
mintmin <mintmin0320@gmail.com>
2024-04-19 17:33:40 +0900

    feat: 투표 메인 페이지 로딩 컴포넌트 적용

--- src/app/(route)/(withLayout)/votes/_component/VotingDashboard.tsx
+++ src/app/(route)/(withLayout)/votes/_component/VotingDashboard.tsx
@@ -4,6 +4,7 @@ import ErrorBoundary from '@/app/_components/errorBoundary'
 import ErrorFullback from '@/app/_components/errorFullback'
 import { QueryErrorResetBoundary } from '@tanstack/react-query'
 import React, { Suspense } from 'react'
+import Loading from '@/app/_components/loading'
 import RankingList from './RankingList'
 import VoteList from './VoteList'
 
@@ -12,7 +13,7 @@ export default function VotingDashboard() {
     <QueryErrorResetBoundary>
       {({ reset }) => (
         <ErrorBoundary onReset={reset} FallbackComponent={ErrorFullback}>
-          <Suspense fallback={<div>Loading. . . </div>}>
+          <Suspense fallback={<Loading />}>
             <RankingList />
             <VoteList />
           </Suspense>
