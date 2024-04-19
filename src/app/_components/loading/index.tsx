9aeb21265de661be9e6dbfd698de236726457034
mintmin <mintmin0320@gmail.com>
2024-04-19 17:32:25 +0900

    feat: 로딩 컴포넌트 추가

---
+++ src/app/_components/loading/index.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Loading() {
+  return (
+    <div className="flex h-dvh justify-center pt-[210px]">
+      <h2 className="text-[22px] font-[700]">Loading. . .</h2>
+    </div>
+  )
+}
