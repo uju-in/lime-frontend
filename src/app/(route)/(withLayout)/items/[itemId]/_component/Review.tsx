c62b4dcec856822d78706defd885975db29d6cb4
mintmin <mintmin0320@gmail.com>
2024-04-30 13:22:32 +0900

    feat: 유저 프로필 조회 router 추가

--- src/app/(route)/(withLayout)/items/[itemId]/_component/Review.tsx
+++ src/app/(route)/(withLayout)/items/[itemId]/_component/Review.tsx
@@ -5,9 +5,7 @@ import Image from 'next/image'
 import useOutsideClick from '@/app/_hook/common/useOutsideClick'
 import { ReviewResponse } from '@/app/_types/review.type'
 import { cn } from '@/app/_utils/twMerge'
-import { dateFormatter } from '../../../../../_utils/dateFormatter'
-
-import StarRating from './StarRating'
+import UserProfile from '@/app/(route)/(withLayout)/items/[itemId]/_component/UserProfile'
 import ReviewModal from './ReviewModal'
 import EditButtons from './EditButtons'
 import ReviewLikeButton from './ReviewLikeButton'
@@ -78,35 +76,7 @@ export default function Review(props: PropsType) {
       >
         {/** 프로필 사진, 닉네임, 날짜, 평점, 레벨 */}
         <div className="flex justify-between">
-          <div className="flex">
-            <Image
-              width={40}
-              height={40}
-              src={memberInfo.profileImage}
-              alt="member profile"
-              className="mr-[8px] h-[40px] w-[40px] rounded-full"
-            />
-            <div>
-              <div className="flex items-center">
-                <p className="mr-[4.52px] text-[12px] font-[700]">
-                  {memberInfo.nickname}
-                </p>
-                <div className="flex h-[13px] w-[27px] justify-center rounded-[4px] bg-[#000]">
-                  <p className="text-[8px] font-[700] text-white">
-                    Lv. {memberInfo.level}
-                  </p>
-                </div>
-              </div>
-              <div className="mt-[4px] flex">
-                {/** 리뷰 별점 */}
-                <StarRating rate={reviewSummary.rate} />
-                <div className="mx-[10px] h-[12px] border-l-[0.5px] border-[#B3B3B3] " />
-                <p className="flex items-center text-[10px] font-[500] text-[#747474]">
-                  {dateFormatter(reviewSummary.createdAt)}
-                </p>
-              </div>
-            </div>
-          </div>
+          <UserProfile memberInfo={memberInfo} reviewSummary={reviewSummary} />
           <div className={cn('hidden', 'mo:block')}>
             <ReviewLikeButton
               reviewId={reviewSummary.reviewId}
