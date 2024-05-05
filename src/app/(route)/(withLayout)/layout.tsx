f9cf323bf98fe99c47d241ab5bda1ffda7e8bff4
mintmin <mintmin0320@gmail.com>
2024-05-05 17:43:02 +0900

    refactor: CookiesProvider 위치 변경

--- src/app/(route)/(withLayout)/layout.tsx
+++ src/app/(route)/(withLayout)/layout.tsx
@@ -1,20 +1,17 @@
 import Footer from '@/app/_components/layout/Footer'
 import Header from '@/app/_components/layout/Header'
 import SearchHeader from '@/app/_components/layout/search/SearchHeader'
-import { CookiesProvider } from 'next-client-cookies/server'
 import React, { ReactNode } from 'react'
 import HomeHeader from './_components/HomeHeader'
 
 export default function WithLayout({ children }: { children: ReactNode }) {
   return (
     <main className="my-0 flex flex-col">
-      <CookiesProvider>
-        <SearchHeader />
-        <Header />
-        <HomeHeader />
-        <div className="mb-[18px]">{children}</div>
-        <Footer />
-      </CookiesProvider>
+      <SearchHeader />
+      <Header />
+      <HomeHeader />
+      <div className="mb-[18px]">{children}</div>
+      <Footer />
     </main>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
+import { CookiesProvider } from 'next-client-cookies/server'
 import RecoilRootProvider from './_components/RecoilRootProvider'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,7 +28,9 @@ export default function RootLayout({
     <html lang="ko">
       <body className={inter.className}>
         <Toaster />
-        <RecoilRootProvider>{children}</RecoilRootProvider>
+        <CookiesProvider>
+          <RecoilRootProvider>{children}</RecoilRootProvider>
+        </CookiesProvider>
       </body>
     </html>
   )
