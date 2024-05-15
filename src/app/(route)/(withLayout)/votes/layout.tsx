8adbe0d649f4744da88f2451b5b4abb45327a8bc
mintmin <mintmin0320@gmail.com>
2024-05-15 23:21:30 +0900

    style: layout 컴포넌트 위치 변경 page -> layout

--- src/app/(route)/(withLayout)/votes/layout.tsx
+++ src/app/(route)/(withLayout)/votes/layout.tsx
@@ -1,9 +1,19 @@
 import RQProvider from '@/app/_components/RQProvider'
+import MoAddButton from '@/app/_components/layout/mobile/MoAddButton'
+import MoNavbar from '@/app/_components/layout/mobile/MoNavbar'
+import MoVoteHeader from './_component/MoVoteHeader'
 
 export default function DetailItemLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <RQProvider>{children}</RQProvider>
+  return (
+    <>
+      <MoVoteHeader />
+      <RQProvider>{children}</RQProvider>
+      <MoAddButton path="votes/add-vote" />
+      <MoNavbar />
+    </>
+  )
 }
--- src/app/(route)/(withLayout)/votes/page.tsx
+++ src/app/(route)/(withLayout)/votes/page.tsx
@@ -1,36 +1,26 @@
-import MoNavbar from '@/app/_components/layout/mobile/MoNavbar'
-import { cn } from '@/app/_utils/twMerge'
-import MoAddButton from '@/app/_components/layout/mobile/MoAddButton'
-import ErrorHandlingWrapper from '@/app/_components/errorHandlingWrapper'
 import ErrorFallback from '@/app/_components/errorFallback'
+import ErrorHandlingWrapper from '@/app/_components/errorHandlingWrapper'
 import Loading from '@/app/_components/loading'
+import { cn } from '@/app/_utils/twMerge'
 import CategoryPicker from '../../../_components/categoryPicker/CategoryPicker'
 import CreateVoteButton from './_component/CreateVoteButton'
-import MoVoteHeader from './_component/MoVoteHeader'
 import RankingList from './_component/RankingList'
 import VoteList from './_component/VoteList'
 
 export default function page() {
   return (
-    <>
-      <main className={cn('mx-auto h-auto h-full w-[794px]', 'mo:w-full')}>
-        <MoVoteHeader />
-        <section
-          className={cn('mt-[52px] flex justify-between', 'mo:pt-[16px]')}
-        >
-          <CategoryPicker path="/votes" />
-          <CreateVoteButton />
-        </section>
-        <ErrorHandlingWrapper
-          fallbackComponent={ErrorFallback}
-          suspenseFallback={<Loading />}
-        >
-          <RankingList />
-          <VoteList />
-        </ErrorHandlingWrapper>
-      </main>
-      <MoAddButton path="votes/add-vote" />
-      <MoNavbar />
-    </>
+    <main className={cn('mx-auto w-[794px]', 'mo:w-full')}>
+      <section className={cn('mt-[52px] flex justify-between', 'mo:pt-[16px]')}>
+        <CategoryPicker path="/votes" />
+        <CreateVoteButton />
+      </section>
+      <ErrorHandlingWrapper
+        fallbackComponent={ErrorFallback}
+        suspenseFallback={<Loading />}
+      >
+        <RankingList />
+        <VoteList />
+      </ErrorHandlingWrapper>
+    </main>
   )
 }
