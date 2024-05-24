e8667c3b40fc142d879beaa79c29183794dc3e28
mintmin <mintmin0320@gmail.com>
2024-05-25 00:36:09 +0900

    refactor: 투표 랭킹 아이템이 없을 경우 코드 분리

--- src/app/(route)/(withLayout)/votes/_component/RankingList.tsx
+++ src/app/(route)/(withLayout)/votes/_component/RankingList.tsx
@@ -12,6 +12,11 @@ export default function RankingList() {
   const { rankingList } = useVoteRanking(hobby)
   const { rankingInfos } = rankingList
 
+  // 투표 랭킹 아이템이 없을 경우
+  if (rankingInfos.length < 0) {
+    return <div className="h-[104px]" />
+  }
+
   return (
     <article className={cn('mt-[52px] w-full', 'mo:pl-[16px]')}>
       <h1 className="text-[26px] font-[700]">투표 랭킹</h1>
@@ -21,32 +26,27 @@ export default function RankingList() {
           'mo:gap-[12px] mo:overflow-x-scroll',
         )}
       >
-        {rankingInfos.length > 0 ? (
-          rankingInfos.map((item) => (
-            <li key={item.id} className="cursor-pointer text-center">
-              <Link href={`/votes/${item.id}`} className="block h-full w-full">
-                <div className="flex h-[104px] w-[104px] items-center justify-center rounded-full border-[2px] border-[#D9D9D9]">
-                  <figure className="flex h-[93px] w-[93px] rounded-full border">
-                    <VoteImage
-                      src={item.item1Image}
-                      innerClassNames="rounded-l-full"
-                    />
-                    <VoteImage
-                      src={item.item2Image}
-                      innerClassNames="rounded-r-full"
-                    />
-                  </figure>
-                </div>
-                <span className="mt-[11.4px] text-[14.2px] font-[500] text-[#5D5D5D]">
-                  {item.participants}명 참여중
-                </span>
-              </Link>
-            </li>
-          ))
-        ) : (
-          // 투표 랭킹 아이템이 없을 경우
-          <div className="h-[104px]" />
-        )}
+        {rankingInfos.map((item) => (
+          <li key={item.id} className="cursor-pointer text-center">
+            <Link href={`/votes/${item.id}`}>
+              <div className="flex h-[104px] w-[104px] items-center justify-center rounded-full border-[2px] border-[#D9D9D9]">
+                <figure className="flex h-[93px] w-[93px] rounded-full border">
+                  <VoteImage
+                    src={item.item1Image}
+                    innerClassNames="rounded-l-full"
+                  />
+                  <VoteImage
+                    src={item.item2Image}
+                    innerClassNames="rounded-r-full"
+                  />
+                </figure>
+              </div>
+              <span className="mt-[11.4px] text-[14.2px] font-[500] text-[#5D5D5D]">
+                {item.participants}명 참여중
+              </span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </article>
   )
