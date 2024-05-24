d0c29ee3a52ed0dd62fac3a2dd217f4d1414c765
mintmin <mintmin0320@gmail.com>
2024-05-25 00:36:52 +0900

    refactor: Link, li 태그 위치 조정

--- src/app/_components/categoryPicker/CategoryPicker.tsx
+++ src/app/_components/categoryPicker/CategoryPicker.tsx
@@ -1,20 +1,22 @@
 'use client'
 
+import { CategoryOption } from '@/app/_constants'
+import useGetSearchParam from '@/app/_hook/common/useGetSearchParams'
 import { cn } from '@/app/_utils/twMerge'
 import Link from 'next/link'
-import useGetSearchParam from '@/app/_hook/common/useGetSearchParams'
-import { CategoryOption } from '@/app/_constants'
 import { defaultCategory } from '../../_utils/defaultCategory'
+
+interface PropsType {
+  path: string
+  innerClassNames?: string
+  isMenu?: boolean
+}
 
 export default function CategoryPicker({
   path,
   innerClassNames,
   isMenu,
-}: {
-  path: string
-  innerClassNames?: string
-  isMenu?: boolean
-}) {
+}: PropsType) {
   const title = useGetSearchParam('title') || '스포츠'
   const category = useGetSearchParam('category') || '농구'
   const menu = useGetSearchParam('menu') || 'MY투표'
@@ -24,7 +26,7 @@ export default function CategoryPicker({
   })
 
   return (
-    <div className="w-full">
+    <div>
       <ul
         className={cn(
           `flex gap-[15px] text-[20px] font-[700] ${innerClassNames}`,
@@ -32,25 +34,26 @@ export default function CategoryPicker({
         )}
       >
         {CategoryOption.map((item) => (
-          <Link
-            href={`${!isMenu ? '?' : `${path}${menu}&`}title=${item.title}&category=${defaultCategory(
-              item.title,
-            )}`}
-            key={item.title}
-          >
-            <li
-              className={cn(
-                'cursor-pointer rounded-[83.333px] px-[16px] py-[8px] text-[#fff]',
-                {
-                  'bg-black text-white mo:rounded-none mo:border-2 mo:border-x-0 mo:border-t-0 mo:border-black mo:bg-white mo:text-black':
-                    title === item.title,
-                  'bg-white text-[#AAA]': title !== item.title,
-                },
-              )}
+          <li key={item.title}>
+            <Link
+              href={`${!isMenu ? '?' : `${path}${menu}&`}title=${item.title}&category=${defaultCategory(
+                item.title,
+              )}`}
             >
-              {item.title}
-            </li>
-          </Link>
+              <div
+                className={cn(
+                  'cursor-pointer rounded-[83px] px-[16px] py-[8px] text-[#fff]',
+                  {
+                    'bg-black text-white mo:rounded-none mo:border-2 mo:border-x-0 mo:border-t-0 mo:border-black mo:bg-white mo:text-black':
+                      title === item.title,
+                    'bg-white text-[#AAA]': title !== item.title,
+                  },
+                )}
+              >
+                {item.title}
+              </div>
+            </Link>
+          </li>
         ))}
       </ul>
       <ul
@@ -66,21 +69,23 @@ export default function CategoryPicker({
               : `?title=${title}&category=${item}`
 
             return (
-              <Link href={link} key={item}>
-                <li
-                  className={cn(
-                    'flex h-[39px] min-w-[60px] cursor-pointer items-center justify-center text-nowrap border-[3px] font-[600]',
-                    'mo:rounded-[100px] mo:px-[14px] mo:py-[10px] mo:text-[12px] mo:font-[500] mo:text-white',
-                    {
-                      'border-x-0 border-t-0 border-b-[#000] mo:bg-black':
-                        category === item,
-                      'border-0 mo:bg-[#AEAEAE]': category !== item,
-                    },
-                  )}
-                >
-                  {item}
-                </li>
-              </Link>
+              <li key={item}>
+                <Link href={link}>
+                  <div
+                    className={cn(
+                      'flex h-[39px] min-w-[60px] cursor-pointer items-center justify-center text-nowrap border-[3px] font-[600]',
+                      'mo:rounded-[100px] mo:px-[14px] mo:py-[10px] mo:text-[12px] mo:font-[500] mo:text-white',
+                      {
+                        'border-x-0 border-t-0 border-b-[#000] mo:bg-black':
+                          category === item,
+                        'border-0 mo:bg-[#AEAEAE]': category !== item,
+                      },
+                    )}
+                  >
+                    {item}
+                  </div>
+                </Link>
+              </li>
             )
           })}
       </ul>
