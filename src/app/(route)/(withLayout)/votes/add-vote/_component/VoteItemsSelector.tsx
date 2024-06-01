fa3bca85a442f9a6bb4b8661a39010cd07136f8c
mintmin <mintmin0320@gmail.com>
2024-06-01 22:16:17 +0900

    refactor: 아이템이 없을 경우 안내 메시지 UI 분리

--- src/app/(route)/(withLayout)/votes/add-vote/_component/VoteItemsSelector.tsx
+++ src/app/(route)/(withLayout)/votes/add-vote/_component/VoteItemsSelector.tsx
@@ -10,33 +10,31 @@ interface PropsType {
   }
 }
 
-export default function VoteItemsSelector(props: PropsType) {
-  const { selectedFolder } = props
+export default function VoteItemsSelector({ selectedFolder }: PropsType) {
+  if (!selectedFolder.itemCount && !selectedFolder.folderId) {
+    return (
+      <div
+        className={cn(
+          'flex h-full flex-col items-center justify-center pt-[160px]',
+          'mo:pt-[250px]',
+        )}
+      >
+        <strong className="mb-[12px] text-[20px] font-[600]">
+          찜한 아이템이 없어요
+        </strong>
+        <span className="text-[14px] font-[500]">
+          마음에 드는 아이템을 담아보세요
+        </span>
+      </div>
+    )
+  }
 
   return (
-    <div>
-      {selectedFolder.itemCount !== 0 && selectedFolder.folderId ? (
-        <>
-          <span className={cn('my-[13px] block text-[12px]', 'mo:hidden')}>
-            아이템 {selectedFolder.itemCount}개
-          </span>
-          <FavoriteList folderId={selectedFolder.folderId} />
-        </>
-      ) : (
-        <div
-          className={cn(
-            'flex h-full flex-col items-center justify-center pt-[160px]',
-            'mo:pt-[250px]',
-          )}
-        >
-          <strong className="mb-[12px] text-[20px] font-[600]">
-            찜한 아이템이 없어요
-          </strong>
-          <span className="text-[14px] font-[500]">
-            마음에 드는 아이템을 담아보세요
-          </span>
-        </div>
-      )}
-    </div>
+    <>
+      <span className={cn('my-[13px] block text-[12px]', 'mo:hidden')}>
+        아이템 {selectedFolder.itemCount}개
+      </span>
+      <FavoriteList folderId={selectedFolder.folderId} />
+    </>
   )
 }
